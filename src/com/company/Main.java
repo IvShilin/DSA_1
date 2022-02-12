5f4eda5254b1e3b0fe17bbcc89866bdfa7fdb284
akmchnkv <miss.thelina26@yandex.ru>
2022-02-12 23:30:39 +0300

    ввзвз

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,81 +1,46 @@
 package com.company;
-class CircularBoundedQueue {
-    private static int front;
-    private static int rear;
-    private static int capacity;
-    private static int[] queue;
 
 
-    CircularBoundedQueue(int size) {
-        front = 0;
-        rear = 0;
-        capacity = size;
-        queue = new int[capacity];
+public class Main implements CircularBoundedQueue {
+    public static void main(int capacity) {
+        int front = -1;
+        int rear = -1;
+        int[] queue = new int[capacity];
     }
 
-    static void isEmpty() {
-        if (front == rear) {
-            System.out.printf("Queue is Empty");
-            return;
-        }
-    }
-    static void insertEl(int item)  {
-        if (capacity == rear) {
-            System.out.println("\nCircular Bounded Queue is full");
-        }
-        else {
-            queue[rear] = item;
-            rear++;
-        }
+    @Override
+    public void offer(Main value) {
+
     }
 
-    static void peek()
-    {
-        if (front == rear) {
-            System.out.println("Queue is Empty\n");
-        }
-        System.out.printf("\nFront Element of the queue: %d", queue[front]);
+    @Override
+    public Main poll() {
+        return null;
     }
 
-    static void offer ( int item){
-        queue[rear] = item;
-        rear++;
+    @Override
+    public void flush() {
+
     }
 
-    static void poll() {
-        for (int i = 0; i < rear - 1; i++) {
-            queue[i] = queue[i + 1];
-        }
-
-        if (rear < capacity) {
-            queue[rear] = 0;
-            rear--;
-        }
+    @Override
+    public boolean isEmpty() {
+        return false;
     }
+
+    @Override
+    public boolean isFull() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public int capacity() {
+        return 0;
+    }
+
 }
-
-
-public class Main {
-    public static void main(String[] args) {
-        CircularBoundedQueue q = new CircularBoundedQueue(4);
-
-
-        q.insertEl(5);
-        q.insertEl(20);
-        q.insertEl(30);
-        q.insertEl(7);
-
-
-        q.peek();
-
-        q.insertEl(90);
-
-        q.poll();
-        q.poll();
-
-
-        q.peek();
-    }
-}
-
-
